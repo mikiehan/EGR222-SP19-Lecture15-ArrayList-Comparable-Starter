1fd4280be96c5e043d0c804ad404ae698958112d
Mi Kyung Han <mhan@calbaptist.edu>
2019-11-06 10:34:38 -0800

    examples

---
+++ src/queue/QueueExample.java
@@ -0,0 +1,13 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        Queue<Integer> numbers = new LinkedList<>();
+        Queue<String> words = new PriorityQueue<>();
+
+    }
+}
