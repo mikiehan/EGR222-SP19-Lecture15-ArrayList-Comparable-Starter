ff40741488f02a1f8ac1c373e2030618634ac4ed
Mi Kyung Han <mhan@example.com>
2020-10-26 20:27:58 -0700

    uncomment codes

--- src/arraylist/ListClient.java
+++ src/arraylist/ListClient.java
@@ -17,28 +17,28 @@ public class ListClient {
         list.add("fun");
         System.out.println(list);
 
-        list.remove("hello"); //remove the first occurrence
-        System.out.println(list);
-
-        list.remove(0); //remove element at index 0
-        System.out.println(list);
-
-        list.remove(2); //remove element at index 2
-        System.out.println(list);
-
-        list.add(3, "xxx"); //insert xxx at index 3
-        System.out.println(list);
-
-        list.add(3, "yyy"); //insert yyy at index 4
-        System.out.println(list);
-
-        list.add(5, "zzz"); //insert zzz at index 5
-        System.out.println(list);
-
-        System.out.println(list.get(4)); //get element at index 7
-        System.out.println(list);
-
-        Collections.shuffle(list); //shuffle elements in list randomly
-        System.out.println(list);
+//        list.remove("hello"); //remove the first occurrence
+//        System.out.println(list);
+//
+//        list.remove(0); //remove element at index 0
+//        System.out.println(list);
+//
+//        list.remove(2); //remove element at index 2
+//        System.out.println(list);
+//
+//        list.add(3, "xxx"); //insert xxx at index 3
+//        System.out.println(list);
+//
+//        list.add(3, "yyy"); //insert yyy at index 4
+//        System.out.println(list);
+//
+//        list.add(5, "zzz"); //insert zzz at index 5
+//        System.out.println(list);
+//
+//        System.out.println(list.get(4)); //get element at index 7
+//        System.out.println(list);
+//
+//        Collections.shuffle(list); //shuffle elements in list randomly
+//        System.out.println(list);
     }
 }
